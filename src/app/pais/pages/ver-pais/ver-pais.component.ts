d3da50ebd840235166b992b1ac471c6009faa378
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-16 17:17:11 -0300

    switchMap -> observable

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-ver-pais',
@@ -16,15 +17,22 @@ export class VerPaisComponent {
   ) { };
 
   ngOnInit(): void {
-    // activatedRoute.params nos da mediante el subscribe le param que viene por el path dinamico de la ruta
     this.activatedRoute.params
-      .subscribe(({ id }) => {
-        console.log(id);
-        this.paisService.getPaisPorAlpha(id)
-          .subscribe(pais => {
-            console.log(pais)
-          })
+      .pipe(
+        switchMap(({ id }) => this.paisService.getPaisPorAlpha(id))
+      )
+      .subscribe(resp => {
+        console.log(resp)
       })
+    // activatedRoute.params nos da mediante el subscribe le param que viene por el path dinamico de la ruta
+    // this.activatedRoute.params
+    //   .subscribe(({ id }) => {
+    //     console.log(id);
+    //     this.paisService.getPaisPorAlpha(id)
+    //       .subscribe(pais => {
+    //         console.log(pais)
+    //       })
+    //   })
   };
 
 }
