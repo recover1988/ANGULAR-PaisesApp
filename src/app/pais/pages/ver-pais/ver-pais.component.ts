77f7078d2e31a87f7066528651991040f05e9375
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-15 23:50:47 -0300

    add subscribe with activatedRoute

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
@@ -7,5 +9,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class VerPaisComponent {
+  // el ActivatedRoute nos permite subscribirnos a cualquier cambio de url
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) { };
+
+  ngOnInit(): void {
+    // activatedRoute.params nos da mediante el subscribe le param que viene por el path dinamico de la ruta
+    this.activatedRoute.params
+      .subscribe(({ id }) => {
+        console.log(id);
+        this.paisService.getPaisPorAlpha(id)
+          .subscribe(pais => {
+            console.log(pais)
+          })
+      })
+  };
 
 }
